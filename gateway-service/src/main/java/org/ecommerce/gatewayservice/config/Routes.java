6cea53cbcac0420d0ebdd0e33f429655490d2c4a
Ziad Abdelnaby <ziadabdelnaby46@gmail.com>
2026-07-17 01:58:39 +0300

    setup config server - discovery service - gateway service

---
+++ gateway-service/src/main/java/org/ecommerce/gatewayservice/config/Routes.java
@@ -0,0 +1,15 @@
+package org.ecommerce.gatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Routes {
+
+//    @Bean
+//    public RouteLocator routes(RouteLocatorBuilder builder) {
+//        return builder.routes().build();
+//    }
+}
