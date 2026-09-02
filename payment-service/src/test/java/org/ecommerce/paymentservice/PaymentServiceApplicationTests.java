13c03127b4efe5c0306cf71e978356de90c4810b
Ziad Abdelnaby <ziadabdelnaby46@gmail.com>
2026-09-02 10:09:05 +0300

    Creating Payment Service Project without implementation

---
+++ payment-service/src/test/java/org/ecommerce/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecommerce.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
